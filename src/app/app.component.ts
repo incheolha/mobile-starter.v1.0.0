dc964347114e321fa39ba368e7a01d89690e2b82
incehol ha <incheolha@gmail.com>
2018-07-24 21:03:36 +0900

    2018.7.23
    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,28 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { WelcomePage } from '../pages/welcome/welcome';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any;
+
+  public pages = [
+    {
+      title: 'Home',
+      icon: 'ios-home-outline',
+      count: 0,
+      component: HomePage
+    }
+  ]
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      this.rootPage = WelcomePage;
       statusBar.styleDefault();
       splashScreen.hide();
     });
